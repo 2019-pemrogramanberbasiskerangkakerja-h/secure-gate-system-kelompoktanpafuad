13749a485ffaca8e55dbecc4cfd59ad35f80a1c9
rohanaq <rohanaq@yahoo.com>
2019-05-16 23:45:23 +0700

    views

--- config/databases.js
+++ config/databases.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
   host : "localhost",
-  user : "cloudy",
-  password : "sembarang12",
+  user : "root",
+  password : "rohana",
   database : "pbkk"
 });
 
